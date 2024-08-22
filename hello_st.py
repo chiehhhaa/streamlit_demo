9f2c465c9f0acab98d217a5814db2d79a2fc8c7f
chiehhhaa <tomato870208@gmail.com>
2024-08-22 12:15:17 +0800

    Add basic Streamlit app for 'Hello, Streamlit' demo

---
+++ hello_st.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import numpy as np
+
+st.title("My first Streamlit Demo!!")
+st.write("Hello, Streamlit!😎")
+
+random_matrix = np.random.randn(10, 20)
+st.write(random_matrix)
+
+k="Hihi"
+k
+bb=123
+bb
+
+random_matrix
